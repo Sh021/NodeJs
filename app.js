e066168960b03ff6569a08ab378bc3bc72454ec8
Shubham <Shubham.glenhill@gmail.com>
2025-01-03 10:56:18 +0530

    new

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
-"use strict";
-const http = require("http");
-
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-//console.log(routes.someText);
+const adminRoutes = require("./routes/admin");
 
-const server = http.createServer(app);
+const shopRoutes = require("./routes/shop");
 
-server.listen(3000);
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/admin',adminRoutes);
+
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send(`<h1>Page not found</h1>`);
+});
+
+app.listen(3000);
